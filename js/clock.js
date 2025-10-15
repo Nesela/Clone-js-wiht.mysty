866811ada22817cd8e0f2c0c03418332d35e00e3
Nesela <100178890+Nesela@users.noreply.github.com>
2025-10-15 18:52:37 +0900

    Add files via upload

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,15 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("5초마다나옴");
+function getClock() {
+    const date =  new Date();
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText =`${hours}:${minutes}:${seconds}`;
 }
 
+getClock(); //새로고침시 시간 바로 불러오기
+setInterval(getClock, 1000); // 1초마다 시간 불러오기
 
-setInterval(sayHello, 5000);
+//interval = 매번 일어나야 하는 무언가!
+//setInterval(실행할 함수, 실행할 함수의 주기)!
